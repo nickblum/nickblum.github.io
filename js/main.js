b1345071264209313cc5b20009a4c8dd8d7b5b9b
Nick Blum <nick.blum@protonmail.com>
2021-11-13 00:14:29 -0500

    Add required field demo

--- js/main.js
+++ js/main.js
@@ -32,6 +32,58 @@ function demoDuration($demoBox){
     $demoRow2.durationjs({display:"hm", mInc:2});
 }
 
-function demoRequired(){
-    alert('required!');
+function demoRequired($demoBox){
+    var $demoLink = $( '<div class="demo-row">' ).append(
+        $('<a class="demo-link" href="https://github.com/nickblum/requiredjs/">').html('[source]')
+    );
+    var $demoRow1 = $( '<div id="demoDurationRow" class="demo-row">' ).append(
+        $('<input id="txt1" placeholder="#txt1">')
+    );
+    var $demoCode1 = $('<code>').html('// Basic requirement<br>'
+        +"var key = 'txt1';<br>"
+        +"reqStruct[key] = {<br>"
+        +"&emsp;field:[addReq(key, key)]<br>"
+        +"};"
+    );
+    var $subsection1 = $('<div class="section-row">').append( $demoRow1, $demoCode1 );
+
+    var $demoRow2 = $( '<div id="demoDurationRow" class="demo-row">' ).append(
+        $('<label>').append($('<input id="chk1" type="checkbox">')).append(document.createTextNode('Chk1')),
+        $('<br>'),
+        $('<label>').append($('<input id="chk2" type="checkbox">')).append(document.createTextNode('Chk2')),
+        $('<br>'),
+        $('<label>').append($('<input id="chk3" type="checkbox">')).append(document.createTextNode('Chk3'))
+    );
+    var $demoCode2 = $('<code>').html('// Alternate requirements<br>'
+        +"var key, id = 'chk1';<br>"
+        +"reqStruct[key] = {<br>"
+        +"&emsp;field:[<br>"
+        +    "&emsp;&emsp;addReq(id,key),<br>"
+        +    "&emsp;&emsp;addReq('chk2',key),<br>"
+        +    "&emsp;&emsp;addReq('chk3',key) ]<br>"
+        +"}"
+    );
+    var $subsection2 = $('<div class="section-row">').append( $demoRow2, $demoCode2 );
+
+    var $demoRow3 = $( '<div id="demoDurationRow" class="demo-row">' ).append(
+        $('<label>').append($('<input id="parent1" type="checkbox">')).append(document.createTextNode('Parent1')),
+        $('<br>'),
+        $('<label>').append($('<input id="child1" placeholder="child1">'))
+    );
+    var $demoCode3 = $('<code>').html('// Dependent requirements<br>'
+    +"var id = 'parent1', key = id;<br>"
+    +"reqStruct[key] = {<br>"
+    +    "&emsp;field:[	addReq(id,key) ],<br>"
+    +    "&emsp;trigger:[{<br>"
+    +        "&emsp;&emsp;field:[	addReq('child1',key) ],<br>"
+    +         "&emsp;&emsp;depend:[ addReq(id,key) ] <br>"
+    +    "&emsp;}]<br>" 
+    +"};<br>"
+    );
+    var $subsection3 = $('<div class="section-row">').append( $demoRow3, $demoCode3 );
+
+    $demoBox.append( $demoLink, $subsection1, $subsection2, $subsection3);
+
+    REQ.init();
+    
 }
--- js/required.js
+++ js/required.js
@@ -38,11 +38,11 @@ var REQ = ( function () {
         	if ( !group.field[i] ) {//skip this loop iteration
         		continue;
         	} else if ( isInput(group.field[i]) ) {// if radio or checkbox, paint parent
-        		 //group.field[i].parentNode.style.backgroundColor = ( group.isRequired ? '#ffc' : '' );
-        		$( group.field[i].parentNode )[ group.isRequired ? 'addClass' : 'removeClass' ]('rbtn_req');
+        		group.field[i].parentNode.style.backgroundColor = ( group.isRequired ? '#ffc' : '' );
+        		//$( group.field[i].parentNode )[ group.isRequired ? 'addClass' : 'removeClass' ]('rbtn_req');
         	} else {// text input or textarea, paint background
-        		//group.field[i].style.backgroundColor = ( group.isRequired ? '#ffc' : '' );
-        		$( group.field[i] )[ group.isRequired ? 'addClass' : 'removeClass' ]('rbtn_req');
+        		group.field[i].style.backgroundColor = ( group.isRequired ? '#ffc' : '' );
+        		//$( group.field[i] )[ group.isRequired ? 'addClass' : 'removeClass' ]('rbtn_req');
         	}
         }
     }
@@ -83,141 +83,40 @@ var REQ = ( function () {
 	}
 
 	return {
-		showIncomplete: function(){
-			var $buttonBox = $( '#csnButtonBox' );
-			var firstIncomplete = false;
-			$buttonBox.find('input').prop('disabled',false);
+		init: function() {
+			var id = 'txt1', key = id;
+			reqStruct[key] = {
+				field:[addReq(id, key)]
+			};
+			
+			// alternates
+			var id = 'chk1', key = id;
+			reqStruct[key] = {
+				field:[	addReq(id,key), addReq('chk2',key), addReq('chk3',key) ]
+			}
+			
+			// dependencies
+			var id = 'parent1', key = id;
+			reqStruct[key] = {
+				field:[	addReq(id,key) ],
+				trigger:[
+					{	field:[	addReq('child1',key) ],
+						depend:[ addReq(id,key) ] 
+					} 
+				] 
+			};
 
+			// run through all required fields, paint as needed
 			for ( var key in reqStruct ){
-                if ( firstIncomplete = findIncomplete( reqStruct[key] ) ) {
-            		var $scrollTarget = $( firstIncomplete );
-            		var pulseTarget = firstIncomplete;
-            		if ( isInput( firstIncomplete ) ){
-            			pulseTarget = firstIncomplete.parentNode;
-            			$scrollTarget = $(( firstIncomplete.type == 'hidden' ? firstIncomplete.parentNode : firstIncomplete ));
-            		}
-                    /*
-                    $("html").animate({ scrollTop: $scrollTarget.offset().top - 200 }, 600, function(){
-            			EMR.UTIL.pulseWarning( pulseTarget );
-                    } );
-                    */
-            		break;
-				}
+                validateGroup(reqStruct[key]);
             }
 
-			return firstIncomplete;
-		},
-		init: function() {
+			/*
+
+			
 			// may need to improve search efficiency with
             /*
-            var $box = $('.csnblock-container');
 
-			// INDIVIDUALS PRESENT
-			$box.find( 'input[id^="csnblock_present_others"]' ).each( function(){
-				var noteID = (this.id).match(/\d+/)[0];
-				var key = this.id;
-
-				reqStruct[key] = {
-					field:[	addReq(key,key),
-							addReq('csnblock_present_patient'+noteID,key) ],
-					trigger:[
-						{	field:[	addReq('csnblock_present_others_txt'+noteID,key)],
-							depend:[ addReq(key,key)]
-						}
-					]
-	            };
-			} );
-
-            
-			// PATIENT REPORT OF PROGRESS
-			$box.find( 'textarea[id^="csnblock_patient_report_txt"]' ).each( function(){
-				reqStruct[this.id] = {
-	                field:[addReq(this.id, this.id)]
-	            };
-			} );
-
-			// PATIENT CONDITION (FOR NON RBHS)
-			$box.find( 'input[id^="csnblock_cnd_mood"][id$="_yes"]' ).each( function(){
-				var noteID = (this.id).match(/\d+/)[0];
-				var key = this.id;
-
-				// mood/affect
-				reqStruct[key] = {
-	                field:[	addReq(key,key),
-	                		addReq('csnblock_cnd_mood'+noteID+'_no',key) ],
-	                trigger:[
-						{	field:[	addReq('csnblock_cnd_mood_txt'+noteID,key)],
-							depend:[ addReq(key,key)] } ] };
-
-				// thought process
-				var key = 'csnblock_cnd_thoughts'+noteID+'_yes'
-				reqStruct[key] = {
-	                field:[	addReq(key,key),
-	                		addReq('csnblock_cnd_thoughts'+noteID+'_no',key) ],
-	                trigger:[
-						{	field:[	addReq('csnblock_cnd_thoughts_txt'+noteID,key)],
-							depend:[ addReq(key,key)] } ] };
-
-				// motivation
-				var key = 'csnblock_cnd_motivation'+noteID+'_yes'
-				reqStruct[key] = {
-	                field:[	addReq(key,key),
-	                		addReq('csnblock_cnd_motivation'+noteID+'_no',key) ],
-	                trigger:[
-						{	field:[	addReq('csnblock_cnd_motivation_txt'+noteID,key)],
-							depend:[ addReq(key,key)] } ] };
-
-				// behavior
-				var key = 'csnblock_cnd_behavior'+noteID+'_yes'
-				reqStruct[key] = {
-	                field:[	addReq(key,key),
-	                		addReq('csnblock_cnd_behavior'+noteID+'_no',key) ],
-	                trigger:[
-						{	field:[	addReq('csnblock_cnd_behavior_txt'+noteID,key)],
-							depend:[ addReq(key,key)] } ] };
-
-				// medical condition
-				var key = 'csnblock_cnd_medical'+noteID+'_yes'
-				reqStruct[key] = {
-	                field:[	addReq(key,key),
-	                		addReq('csnblock_cnd_medical'+noteID+'_no',key) ],
-	                trigger:[
-						{	field:[	addReq('csnblock_cnd_medical_txt'+noteID,key)],
-							depend:[ addReq(key,key)] } ] };
-
-				// substance
-				var key = 'csnblock_cnd_substance'+noteID+'_yes'
-				reqStruct[key] = {
-	                field:[	addReq(key,key),
-	                		addReq('csnblock_cnd_substance'+noteID+'_no',key) ],
-	                trigger:[
-						{	field:[	addReq('csnblock_cnd_substance_txt'+noteID,key)],
-							depend:[ addReq(key,key)] } ] };
-
-			} );
-
-			// PATIENT CONDITION (FOR RBHS)
-			$box.find( 'input[id^="csnblock_rbhs_mood"][id$="_yes"]' ).each( function(){
-				var noteID = (this.id).match(/\d+/)[0];
-				var key = this.id;
-
-				// mood/affect
-				reqStruct[key] = {
-	                field:[	addReq(key,key),
-	                		addReq('csnblock_rbhs_mood'+noteID+'_no',key) ],
-	                trigger:[
-						{	field:[	addReq('csnblock_rbhs_mood_txt'+noteID,key)],
-							depend:[ addReq(key,key)] } ] };
-
-				// behavior
-				var key = 'csnblock_rbhs_behavior'+noteID+'_yes'
-				reqStruct[key] = {
-	                field:[	addReq(key,key),
-	                		addReq('csnblock_rbhs_behavior'+noteID+'_no',key) ],
-	                trigger:[
-						{	field:[	addReq('csnblock_rbhs_behavior_txt'+noteID,key)],
-							depend:[ addReq(key,key)] } ] };
-			} );
 
 			// RISK ASSESSMENT
 			$box.find( 'input[id^="csnblock_si_thoughts"][id$="_yes"]' ).each( function(){
@@ -257,40 +156,7 @@ var REQ = ( function () {
 					]
 	            };
 
-				// "Suicidal Behavior" and sub requirements
-				reqStruct[key2] = {
-	                field:[	addReq(key2,key2),
-	                		addReq('csnblock_si_behavior'+noteID+'_no',key2)],
-	                trigger:[
-            			{	field:[	addReq('csnblock_si_behavior_txt'+noteID,key2)],
-							depend:[addReq(key2,key2)]
-						},
-						trigStruct
-					]
-	            };
-			} );
 
-
-			// FOCUS OF SESSION
-			$box.find( 'textarea[id^="csnblock_focus_txt"]' ).each( function(){
-				var noteID = (this.id).match(/\d+/)[0];
-				var key = this.id;//key can be anything, just needs to be unique.
-				var fieldArr = [addReq(this.id,key)];
-
-				$( 'input[id^="csnblock_objective'+noteID+'"]' ).each( function(){
-					fieldArr.push( addReq(this.id,key) );
-				});
-
-				reqStruct[key] = {
-	                field:fieldArr
-	            };
-			} );
-
-			// CONTENT OF SESSION
-			$box.find( 'textarea[id^="csnblock_ci_content_txt"]' ).each( function(){
-				reqStruct[this.id] = {
-	                field:[addReq(this.id, this.id)]
-	            };
 			} );
 
 			// INTERVENTIONS (POC)
@@ -311,129 +177,7 @@ var REQ = ( function () {
 				reqStruct[this.id] = {
 	                field:[addReq(this.id, this.id)]
 	            };
-
-			} );
-
-			// THEREPEUTIC INTERVENTIONS (CPSS/H059)
-			$box.find( 'textarea[id^="csnblock_int_describe_txt"]' ).each( function(){
-				var noteID = (this.id).match(/\d+/)[0];
-
-				var key = 'csnblock_int_informed_choices'+noteID;
-				if ( $( "#" + key ).length ){
-					reqStruct[key] = {
-						field:[	addReq(key,key),
-								addReq('csnblock_int_self_worth'+noteID,key),
-								addReq('csnblock_int_self_help'+noteID,key),
-								addReq('csnblock_int_make_contact'+noteID,key),
-								addReq('csnblock_int_proactive_role'+noteID,key),
-								addReq('csnblock_int_crisis_plan'+noteID,key),
-								addReq('csnblock_int_positive_communication'+noteID,key),
-								addReq('csnblock_int_ada_accomodations'+noteID,key)
-						]
-		            };
-	            }
-	            reqStruct[this.id] = {
-	                field:[addReq(this.id, this.id)]
-	            };
-			} );
-
-			// RESPONSE
-			$box.find( 'textarea[id^="csnblock_response_txt"]' ).each( function(){
-				var noteID = (this.id).match(/\d+/)[0];
-
-				var key = 'csnblock_response_cooperative'+noteID;
-				if ( $( "#" + key ).length ){
-					reqStruct[key] = {
-						field:[	addReq('csnblock_response_attentive'+noteID,key),
-								addReq('csnblock_response_cooperative'+noteID,key),
-								addReq('csnblock_response_suspicious'+noteID,key),
-								addReq('csnblock_response_agreeable'+noteID,key),
-								addReq('csnblock_response_open'+noteID,key),
-								addReq('csnblock_response_guarded'+noteID,key),
-								addReq('csnblock_response_agitated'+noteID,key),
-								addReq('csnblock_response_refused'+noteID,key),
-								addReq('csnblock_response_aggressive'+noteID,key),
-								addReq('csnblock_response_disinterested'+noteID,key),
-								addReq('csnblock_response_distracted'+noteID,key)
-						]
-		            };
-	            }
-
-	            reqStruct[this.id] = {
-	                field:[addReq(this.id, this.id)]
-	            };
-
-			} );
-
-			// PATIENT PROGRESS/RESPONSE TO INTERVENTIONS
-			$box.find( 'textarea[id^="csnblock_progress_txt"]' ).each( function(){
-				reqStruct[this.id] = {
-	                field:[addReq(this.id, this.id)]
-	            };
-			} );
-
-			// ACTION PLAN
-			$box.find( 'textarea[id^="csnblock_action_txt"]' ).each( function(){
-				reqStruct[this.id] = {
-	                field:[addReq(this.id, this.id)]
-	            };
-			} );
-
-			// SPD RECOMMENDATIONS
-			$box.find( 'textarea[id^="csnblock_spd_recommendations_txt"]' ).each( function(){
-				reqStruct[this.id] = {
-	                field:[addReq(this.id, this.id)]
-	            };
-			} );
-
-			// PATIENT STATUS
-			$box.find( 'textarea[id^="csnblock_spd_progress_txt"]' ).each( function(){
-				reqStruct[this.id] = {
-	                field:[addReq(this.id, this.id)]
-	            };
-			} );
-
-			// DISPOSITION
-			$box.find( 'textarea[id^="csnblock_ci_disposition_txt"]' ).each( function(){
-				reqStruct[this.id] = {
-	                field:[addReq(this.id, this.id)]
-	            };
-			} );
-
-			// GROUP ACTIVITIES
-			$box.find( 'textarea[id^="csnblock_group_activities_txt"]' ).each( function(){
-				reqStruct[this.id] = {
-	                field:[addReq(this.id, this.id)]
-	            };
-			} );
-
-			// PRIMARY CARE
-			$box.find( 'input[type="hidden"][id^="csnblock_pc_complete"]' ).each( function(){
-				reqStruct[this.id] = {
-	                field:[addReq(this.id, this.id)]
-	            };
-			} );
-
-			// NOTES
-			$box.find( 'textarea[id^="csnblock_note_txt"]' ).each( function(){
-				var noteID = (this.id).match(/\d+/)[0];
-				reqStruct[this.id] = {
-	                field:[	addReq(this.id, this.id),
-	                		addReq("csnblock_compl_ica" + noteID, this.id) ]
-	            };
-
-			} );
-
-			// EBP SELECT
-			$box.find( 'select[id^="csnblock_ebp_id"]' ).each( function(){
-				reqStruct[this.id] = { field:[addReq(this.id, this.id)] };
-			} );
-
-			// run through all required fields, paint as needed
-			for ( var key in reqStruct ){
-                validateGroup(reqStruct[key]);
-            }
-            */
+			} );*/
 		}
 	}
 }());
